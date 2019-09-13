b72c747d4acefce1cdf317882ae1d45fd5846011
iarani5 <iaranizza5@gmail.com>
2019-09-13 09:39:59 -0300

    editar cancha v1

--- js/controllers/controller-mis-canchas.js
+++ js/controllers/controller-mis-canchas.js
@@ -43,6 +43,46 @@
 		});
 	}
 	
+	//EDITAR CANCHA
+	$scope.editar_cancha=function(cancha){
+			
+		//parsear fecha
+		var parts = cancha.FECHA_VENCIMIENTO_TARJETA.split('-');
+		var mydate = new Date(parts[0], parts[1] - 1, parts[2]); 
+
+		//direccion
+		id("pac-input").value=cancha.DIRECCION;
+
+		$scope.cancha={
+			NOMBRE: cancha.NOMBRE_CANCHA,
+			//FOTO: cancha.FOTO,
+			TIPO_CANCHA: cancha.TIPO_CANCHA,
+			//LONGITUD: cancha.LONGITUD,
+			//LATITUD: cancha.LATITUD,
+			//DIRECCION: cancha.DIRECCION,
+			TARJETA: parseInt(cancha.TARJETA, 10),
+			CLAVE: parseInt(cancha.CLAVE_TARJETA, 10),
+			FECHA_VENCIMIENTO_TARJETA: mydate,
+			PRECIO: parseInt(cancha.PRECIO, 10)
+		}
+		
+			
+		/*$http({
+			method: 'POST',
+			url: "php/abm/editar.cancha.php",
+			//data: "ID_CANCHA="+id,			 
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+		})
+		.then(function (response){
+			if(response.data=="1"){
+				alert("cancha eliminada con éxito");
+				$window.location.reload();
+			}
+		},function (error){ //ERROR no se pudo establecer la conexion
+
+		});*/
+	}
+	
 	//*************************** CREAR CANCHA
 	
 	var search =id("pac-input");
@@ -124,81 +164,5 @@
 			alert("Formulario incompleto, todos los datos son requeridos");
 		} 
 	}
-	
-	
-	
-	/*var cities = [
-              {
-                  city : 'India',
-                  desc : 'This is the best country in the world!',
-                  lat : 23.200000,
-                  long : 79.225487
-              },
-              {
-                  city : 'New Delhi',
-                  desc : 'The Heart of India!',
-                  lat : 28.500000,
-                  long : 77.250000
-              },
-              {
-                  city : 'Mumbai',
-                  desc : 'Bollywood city!',
-                  lat : 19.000000,
-                  long : 72.90000
-              },
-              {
-                  city : 'Kolkata',
-                  desc : 'Howrah Bridge!',
-                  lat : 22.500000,
-                  long : 88.400000
-              },
-              {
-                  city : 'Chennai  ',
-                  desc : 'Kathipara Bridge!',
-                  lat : 13.000000,
-                  long : 80.250000
-              }
-          ];
-		  
-			var mapOptions = {
-                  zoom: 12,
-                  center: new google.maps.LatLng(-34.568101,-58.4709995),
-                  mapTypeId: google.maps.MapTypeId.TERRAIN
-              }
-
-              $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-              $scope.markers = [];
-			  
-			
-              
-              var infoWindow = new google.maps.InfoWindow();
-              
-              var createMarker = function (info){
-                  
-                  var marker = new google.maps.Marker({
-                      map: $scope.map,
-                      position: new google.maps.LatLng(info.lat, info.long),
-                      title: info.city
-                  });
-                  marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
-                  
-                  google.maps.event.addListener(marker, 'click', function(){
-                      infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
-                      infoWindow.open($scope.map, marker);
-                  });
-                  
-                  $scope.markers.push(marker);
-                  
-              }  
-              
-              for (i = 0; i < cities.length; i++){
-                  createMarker(cities[i]);
-              }
-
-              $scope.openInfoWindow = function(e, selectedMarker){
-                  e.preventDefault();
-                  google.maps.event.trigger(selectedMarker, 'click');
-              }*/
 }]);
 	
